75ab05f050cf4e1247004b49bab22911cb31770f
BeastBear <phurich_10@hotmail.com>
2022-08-26 11:31:12 +0700

    add class-props.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React from 'react'
-import {Header, Content, Footer} from './func-component'
+//import {Header, Content, Footer} from './func-component'
+//import Calendar from './class-component'
+import MsgBox from './class-props'
 
 function App(){
+  /*
   return(
     [<Header/>,<Content/>,<Footer/>]
   )
+  */
   /*
   return (
     <>
       <Header/>
+      <p><center><Calendar/></center></p>
       <Content/>
       <Footer/>
     </>
   )
   */
+  return (
+    <MsgBox
+      text="React & React Native"
+      color="red"
+      bgColor="#ccc"
+      fontsize="18pt"
+      border="solid 1px black"
+    />
+  )
 }
 export default App
